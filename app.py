5b4d19ecf31c01c315e6a651e2006d46eeea6277
mursalinsaiyed <mursalinsaiyed@gmail.com>
2024-05-17 17:10:08 -0400

    Use gunicorn and proper port binding for Heroku

--- app.py
+++ app.py
@@ -132,4 +132,4 @@ def scan():
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    app.run(debug=True, host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port)
